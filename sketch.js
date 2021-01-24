710e368a3cd49b4aba7d677012d55a50179951e5
Chirag Khatri <74537319+Chirag963@users.noreply.github.com>
2021-01-24 10:17:01 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var hero, monster;
+var background;
+var fly;
+
+function preload() {
+//preload the images here
+hero = loadImage("images/Superhero-01.png, Superhero-02.png");
+}
+
+function setup() {
+  createCanvas(3000, 800);
+  // create sprites here 
+  
+
+}
+
+function draw() {
+  background(0);
+
+}
+
